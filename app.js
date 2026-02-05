6e2abca97e1e1ede70410c13071d51cba128ace1
Mastewal <mastewalgshiferaw@gmail.com>
2026-02-05 22:22:25 +0300

    first hello world

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express'); //express module imported
+const app = express(); //express app created (instance)
+const port = 3000; //port number defined
+
+
+//define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello World!'); //send response
+});
+
+//start the server and listen and listen for incoming requests
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
